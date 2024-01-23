fe6d74bdb67ca276e1fe9f52daaa07d5aa694c84
Michael <81646678+MichaelGats@users.noreply.github.com>
2024-01-22 23:01:19 -0500

    Created level 1-1
    - Created level 1-1 scene
    - Added colliders and mapped the level
    - Added enemies

--- MQP-2D-Platformer/Assets/Scripts/Portal.cs
+++ MQP-2D-Platformer/Assets/Scripts/Portal.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; 
 
 public class Portal : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+
+    private void OnMouseDown()
     {
-        
+        Debug.Log("Mouse clicked on portal 1-1");
+        SceneManager.LoadScene("Level 1-1");
     }
 }
